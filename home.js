1447fa09053f6db70e967484c28dd82a51e9fdcd
Pauline Mae Ona <paulinemaeona@gmail.com>
2023-11-06 15:45:38 +0800

    feat: My first feature

---
+++ home.js
@@ -0,0 +1,194 @@
+// let person = {
+//     firstName: 'John',
+//     lastName: 'Doe'
+// };
+
+// personList = [person, {
+//     firstName: 'Jane',
+//     lastName: 'Doe'
+// }, {
+//         firstName: 'Jack',
+//         lastName: 'Doe'
+//     }, {
+//         firstName: 'Justin',
+//         lastName: 'Smith'
+//     }];
+
+// console.log(personList[0]);
+
+
+// message = 'Hello World';
+
+// showMesssage(message);
+
+
+// let state = "LA";
+// let taxPercent;
+
+// if (state === "CA") {
+//     taxPercent = 7.25;
+// }
+
+// else if (state === "LA") {
+//     taxPercent = 9.25;
+// }
+
+// console.log(taxPercent);
+
+
+// shortcut for if/else (ternary operator)
+// 3 conditions
+// price = 15;
+
+// let message = (price > 10) ? 'expensive' : 'cheap';
+
+// showMesssage(message);
+
+
+// for loop
+// for (let i = 0; i <= 5; i++) {
+//     console.log(i);
+// }
+
+//while loop
+
+// let count = 1;
+
+// while (count < 5) {
+//     console.log(count);
+//     count++;
+// }
+
+
+let myfunction = function logMessage(message, anotherMessage) {
+    console.log(message, anotherMessage);
+    console.log(message);
+    console.log(anotherMessage);
+
+}
+
+// myfunction('Test', 'Another Test'); // Test Another Test
+// myfunction('John'); // John undefined
+
+
+
+// function getSecretCode(value) {
+//     let code = value * 42;
+//     return code;
+// }
+
+// let secretCode = getSecretCode(2);
+// showMessage(secretCode); // 84
+
+// console.log(code); // 84
+
+
+// let key = 42;
+
+// function getSecretCode(value) {
+
+//     let keyGenerator = function () {
+//         let key = 12;
+//         console.log('in keyGenerator: ', key);
+//         return key;
+//     }
+
+//     let code = value * keyGenerator();
+//     console.log(' in getSecretCode: ', key);
+//     return code;
+// }
+
+// let secretCode = getSecretCode(2);
+// showMessage(secretCode); // 24
+
+changePercentOff(32);
+
+// let mySymbol = Symbol();
+
+// let person = {
+//     name: 'John',
+//     age: 32,
+//     partTime: false,
+//     message: 'Hello World',
+//     [mySymbol]: 'secretInformation',
+//     showInfo: function (realAge) {
+//         showMesssage(this.name + ' is ' + realAge + ' years old');
+//     }
+// };
+
+// person.showInfo(100)
+
+// console.log(typeof person.showInfo)
+
+// let message = 'Hello';
+
+// function changeMessage(message) {
+//     message = 'Hi!';
+//     return message;
+// }
+
+// message = changeMessage(message);
+
+// showMesssage(message); // Hello
+
+// console.log(message); // Hi!
+
+
+// let person = {
+//     name: "John",
+//     age: 32,
+//     partTime: false,
+// };
+
+// function incrementAge(person) {
+//     person.age++;
+// }
+
+// incrementAge(person);
+// console.log(person.age); // 33
+// showMesssage(person.age); // 33
+
+
+// let now = new Date();
+// showMesssage(now.toDateString());
+
+// console.log(Math.random()); // 42
+
+
+// let numstr = "100";
+
+// stringtoNumber = Number.parseFloat(numstr);
+
+// console.log(typeof stringtoNumber); // 10
+
+// Styling the Webpage with JavaScript
+
+// Showing and Hiding DOM Elements
+
+const header = document.getElementById('message');
+
+header.style.color = '#00000';
+header.style.fontWeight = '900';
+
+const button = document.getElementById('see-review');
+
+button.addEventListener('click', function () {
+    console.log('click');
+    const review = document.getElementById('review');
+
+    if (review.classList.contains('d-none')) {
+        review.classList.remove('d-none');
+        button.textContent = 'Close Review';
+    }
+    else {
+        review.classList.add('d-none');
+        button.textContent = 'See Review';
+    }
+});
+
+
+const values = ['a', 'b', 'c'];
+
+console.log(Array.isArray(values));
+
+
---
+++ utils.js
@@ -0,0 +1,18 @@
+//This function changes the message on the page with an id of 'message'
+function showMesssage(message) {
+    document.getElementById("message").textContent = message;
+}
+
+// document  - javascript object that represents the html document
+// getElementById - function that returns an element from the html document
+// message - the id of the element we want to return
+// textContent - property of the element we want to change
+// mesaage - the value we want to set the textContent property to
+
+
+
+
+//This function changes the percentage off on the page with an id of 'percent-off'
+function changePercentOff(percentage) {
+    document.getElementById("percent-off").textContent = percentage + "% OFF";
+}
